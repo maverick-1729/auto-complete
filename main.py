5a8a47439045610283fb7b3a287e103a64c130e0
maverick-1729 <bhavyasavalia0205@gmail.com>
2025-02-12 22:49:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ import autocomplete_trie
 
 app = FastAPI()
 
-class InputData(BaseModel):
+class InputData_buy(BaseModel):
     text: str
 
-@app.post("/autocomplete/")
-def predict(data: InputData):
-    result = autocomplete_trie.get_suggestions_api(data.text)  # Call your function
+class InputData_sell(BaseModel):
+    texts: list[str]
+    query: str
+
+@app.post("/autocomplete_buy/")
+def predict(data: InputData_buy):
+    result = autocomplete_trie.get_suggestions_api_buy(data.text)  # Call your function
+    return {"suggestions": result}
+
+@app.post("/autocomplete_sell/")
+def predict(data: InputData_sell):
+    result = autocomplete_trie.get_suggestions_api_sell(data.query, data.texts)  # Call your function
     return {"suggestions": result}
